219b3c044284ef5994b88be3219a343e8d2b2b59
Gabriela <gabrielanunesdaliaa@gmail.com>
2026-08-21 18:01:01 -0300

    Update CadastroLivros.py

--- CadastroLivros.py
+++ CadastroLivros.py
@@ -1,10 +1,30 @@
-
 def cadastro():
     nome = input('Nome do livro: ')
     autor = input('Autor: ')
     ano = int(input('Ano: '))
     qt = int(input('Quantidade em estoque: '))
-    with open ("livros.txt", "a") as arquivo:
-        arquivo.write(f"{nome};{autor};{ano};{qt}")
+    with open("livros.txt", "r") as arquivo:
+        linhas = arquivo.readlines()
 
-cadastro()
+        for linha in linhas:
+            dados = linha.strip().split(';')
+            if dados[0] == nome and dados[1] == autor and int(dados[2]) == ano:
+                print("Esse livro já foi cadastrado anteriormente! ")
+                return
+    with open("livros.txt", "a") as arquivo:
+        arquivo.write(f"{nome};{autor};{ano};{qt}\n")
+        print("Livro cadastrado com sucesso!")
+
+def listarLivros():
+    with open("livros.txt", "r") as arquivo:
+        linhas = arquivo.readlines()
+        if not linhas:
+            print("Não há livros cadastrados no sistema!")
+        else:
+            for linha in linhas:
+                dados = linha.strip().split(';')
+                print(f"Livro:{dados[0]}")
+                print(f"Autor:{dados[1]}")
+                print(f"Ano:{dados[2]}")
+                print(f"Exemplares disponíveis:{dados[3]}")
+                print("-"*25)
